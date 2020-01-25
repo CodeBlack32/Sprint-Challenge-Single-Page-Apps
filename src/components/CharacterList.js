3f3ce3b748f44283defaef2892688d058f037fc6
Jarvis Black <usnblack@gmail.com>
2020-01-25 12:09:52 -0500

    stuck on .map

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,49 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default function CharacterList() {
+const CharacterList = props => {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getCharacters = () => {
+      axios
+        .get(`https://rickandmortyapi.com/api/character`)
+        .then(response => {
+          setCharacters(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    };
+    getCharacters();
   }, []);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <div className="character-list">
+      {characters.map(character => (
+        <CharacterDetails key={character.id} character={character} />
+      ))}
+    </div>
+  );
+};
+
+function CharacterDetails({ character }) {
+  const { name, species, status } = character;
+  return (
+    <Link to={`/components/${character.id}`} className="character-card">
+      <h2>{name}</h2>
+      <div className="character-species">
+        Species: <em>{species}</em>
+      </div>
+      <div className="character-status">
+        Status: <strong>{status}</strong>
+      </div>
+    </Link>
   );
 }
+
+export default CharacterList;
